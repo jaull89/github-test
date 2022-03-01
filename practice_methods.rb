0d0791a5a3ff8fc21280a43c953318cf9186a4da
Joe Aull <jaull89@gmail.com>
2022-03-01 12:27:34 -0600

    add practice_methods file

---
+++ practice_methods.rb
@@ -0,0 +1,58 @@
+# 1.
+# puts "Enter a word"
+
+# word = gets.chomp
+
+# puts word.upcase
+
+# 2.
+# puts "Enter a number"
+
+# number = gets.chomp
+
+# if number.to_i > 100
+#   puts "Thats a big number"
+# end
+
+# 3.
+# puts "Enter a number"
+# number1 = gets.chomp
+
+# puts "Enter another number"
+# number2 = gets.chomp
+
+# sum = number1.to_i + number2.to_i
+
+# p sum
+
+# 4.
+# puts "Enter a word"
+
+# word = gets.chomp
+
+# puts word.reverse
+
+# 5. 
+# puts "Enter a number"
+
+# number = gets.chomp
+
+# puts number.to_i * 10
+
+# 6.
+# puts "Enter a word"
+
+# word1 = gets.chomp
+
+# puts "Enter another word"
+
+# word2 = gets.chomp
+
+# puts word1.upcase + word2.upcase
+
+# 7.
+# puts "Enter a word"
+
+# word = gets.chomp
+
+# puts word.count word
